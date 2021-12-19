32359e410da94e865c9d97f01c0b5bb08be706e3
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-12-19 12:28:46 +0100

    add abstuct method

--- src/main/java/com/teamyostrik/easystock/repository/ArticleRepository.java
+++ src/main/java/com/teamyostrik/easystock/repository/ArticleRepository.java
@@ -2,7 +2,17 @@ package com.teamyostrik.easystock.repository;
 
 import com.teamyostrik.easystock.models.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Integer, Article> {
-    Article findArticleByCode(String code);
+    @Query("select a from article where code_article = :code and designation = :designation")
+    List<Article> findByCustomQuery(@Param("code") String code, @Param("designation") String designation);
+
+    @Query(value="select * from article where code_article = :code", nativeQuery = true)
+    List<Article> findByCustomQuery(@Param("code") String code);
+
+    List<Article> findByCodeArticleIgnoreCaseAndDesignationIgnoreCase(String codeArticle, String designation);
 }
