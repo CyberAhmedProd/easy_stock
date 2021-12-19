d7a34e0c808e0e85e8a2f3eafb12aa55eebc0c20
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-12-19 16:46:46 +0100

    add skull of the controller for article

--- src/main/java/com/teamyostrik/easystock/controllers/ArticleController.java
+++ src/main/java/com/teamyostrik/easystock/controllers/ArticleController.java
@@ -1,14 +1,41 @@
 package com.teamyostrik.easystock.controllers;
 
 
+import com.teamyostrik.easystock.dto.ArticleDto;
+import com.teamyostrik.easystock.models.Article;
 import com.teamyostrik.easystock.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import com.teamyostrik.easystock.utils.Constants;
+
+import java.util.List;
 
 @RestController
 public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
+
+    @PostMapping(value = Constants.APP_ROOT+"/article/create",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArticleDto save(Article article)
+    {
+        return null;
+    }
+    @GetMapping(value = Constants.APP_ROOT+"/article",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ArticleDto> getAll()
+    {
+        return null;
+    }
+    @PutMapping(value = Constants.APP_ROOT+"/article/{id}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArticleDto update(){
+        return null;
+    }
+    @DeleteMapping(value = Constants.APP_ROOT+"/article/{id}")
+    public void delete(@RequestParam Integer id)
+    {
+
+    }
+
+
 }
---
+++ src/main/java/com/teamyostrik/easystock/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.teamyostrik.easystock.utils;
+
+public interface Constants {
+
+    public static String APP_ROOT = "easystock/v0/";
+}
