3038808fbfcbbe5f534dd0f45a5d0acbed5dab23
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-12-19 10:30:23 +0100

    add excpetions

--- src/main/java/com/teamyostrik/easystock/exceptions/InvalidEntityExceptions.java
+++ src/main/java/com/teamyostrik/easystock/exceptions/InvalidEntityExceptions.java
@@ -1,12 +1,17 @@
 package com.teamyostrik.easystock.exceptions;
 
 public class InvalidEntityExceptions extends RuntimeException{
+    private ErrorCode errorCode;
     public InvalidEntityExceptions(String message) {
         super(message);
     }
-
     public InvalidEntityExceptions(String message, Throwable cause) {
         super(message, cause);
+    }
+    public InvalidEntityExceptions(String message , Throwable cause , ErrorCode errorCode)
+    {
+        super(message,cause);
+        this.errorCode = errorCode;
     }
 
 }
