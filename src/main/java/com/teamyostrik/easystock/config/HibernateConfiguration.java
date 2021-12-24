4aa05623fb470cdb1e443096a1a876ac8aeef20b
CyberAhmedProd <ahmed.elgharbi@polytechnicien.tn>
2021-12-24 16:32:49 +0100

    fix creation date and updates for entities

---
+++ src/main/java/com/teamyostrik/easystock/config/HibernateConfiguration.java
@@ -0,0 +1,10 @@
+package com.teamyostrik.easystock.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class HibernateConfiguration {
+}
